055dc49c81db06104a734fbce620b6068bfc2dae
BeG-by <istokx15@gmail.com>
2021-02-25 01:17:11 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+let isCorrect = (arr) => arr !== undefined && arr.length !== 0;
+let sort = (arr) => arr.sort((a, b) => a - b);
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    return isCorrect(array) ? sort(array)[0] : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return isCorrect(array) ? sort(array)[array.length - 1] : 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return isCorrect(array) ? array.reduce((a, b) => a + b) / array.length : 0;
+};
